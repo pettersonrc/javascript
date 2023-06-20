b3d199d777051cf7b7e2e611973d8a49a4251a01
Petterson Rodolpho da Cunha <pettersondacunharodolpho@gmail.com>
2023-06-20 18:16:01 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.5
-console.log()
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
