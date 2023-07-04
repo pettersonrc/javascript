4a73ca182bfdf84cb2a2acfae5fdc44cc12de9ab
Petterson Rodolpho da Cunha <pettersondacunharodolpho@gmail.com>
2023-07-04 18:08:52 -0300

    update ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,9 +6,9 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) { // VALIDAÇÃO
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByName('radsex')[]
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada ${idade}`
+        
     }
 
 }
