6a803409a1e966b4ff8e48bd5f69a578967ce657
Petterson Rodolpho da Cunha <pettersondacunharodolpho@gmail.com>
2023-07-08 13:17:04 -0300

    ex015 update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,11 +3,40 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) { // VALIDAÇÃO
+    if (fano.value.length == 0 || fano.value > ano) { // VALIDAÇÃO
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-homem.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-homem.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-homem.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-homem.jpg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-mulher.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-mulher.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-mulher.jpg')
+            } else {
+                img.setAttribute('src', 'idosa-mulher.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
         
     }
 
