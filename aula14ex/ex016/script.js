eb0dc7fd7b4556c3e7e8d1f0975085a538bff205
Petterson Rodolpho da Cunha <pettersondacunharodolpho@gmail.com>
2023-08-07 15:44:16 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,12 +8,13 @@ function contar() {
         res.innerHTML = 'Impossível contar!'
     } else {
 
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0) {
-            window.alert('Passo inválido!')
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
         if (i < f) {
             // Contagem crescente
